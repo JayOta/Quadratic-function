4c27b8bb93bce11ccda6ecfc12951d7a03e455fb
JayOta <otavio.cvilela@gmail.com>
2025-08-07 12:33:29 -0300

    Enjoy <3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <cmath>
+#include <map>
 using namespace std;
 
 void quadratic_function() {
@@ -29,3 +30,20 @@ int main()
     return 0;
 }
 
+int delta(int a, int b, int c) {
+ int delta = pow(b, 2) - 4 * a * c;
+ int ac = a * c;
+ string b_passo2 = b << "²"; // Arrumar
+ map<int, string> passo1 = {{a, "a"}, {b, "b²"}, {c, "c"}};
+ map<int, int> passo2 = {{a, a}, {b, b}, {c, c}};
+ map<int, int> passo3 = {{a, a}, {b, pow(b, 2)}, {c, c}};
+ map<int, int> passo4 = {{ac, ac}, {b, b}};
+
+ // Pensar em como acessar ->
+ cout << "Δ = " << passo1[0][1] << "-4 * " << passo1[0][0] << " * " << passo1[0][2];
+ cout << "Δ = " << passo2[0][1] << "-4 * " << passo2[0][0] << " * " << passo2[0][2];
+ cout << "Δ = " << passo3[0][1] << "-4 * " << passo2[0][0] << " * " << passo2[0][2];
+
+
+ 
+}
