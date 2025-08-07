e84f26b7739a7ff85dd6158fa1871ddad24b1a49
JayOta <otavio.cvilela@gmail.com>
2025-08-07 12:04:08 -0300

    Enjoy <3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <list>
 #include <cmath>
 using namespace std;
 
+void quadratic_function() {
+    int a, b, c;
+    cout << "Insira o Primeiro Elemento(ax²): " << " "<< endl;
+    cin >> a;
+    cout << "Insira o Segundo Elemento(bx): " << endl;
+    cin >> b;
+    cout << "Insira o Terceiro Elemento(c): " << endl;
+    cin >> c;
+    
+    int delta = pow(b,2) - 4 * a * c;
+    
+    int x1 = (-b - sqrt(delta)) / (2 * a);
+    int x2 = (-b + sqrt(delta)) / (2 * a);
+    
+    cout << "X1 = " << x1 << " , X2 = " << x2 << endl;
+}
+
 int main()
 {
+    quadratic_function();
     
-    std::cout<<"Hello World";
-
     return 0;
 }
 
-int quadratic_function(a, b, c) {
-    float delta = pow(b,2) - 4 * a * c;
-    
-    x1 = (-b + sqrt(delta)) / 2 * a;
-    x2 = (-b - sqrt(delta)) / 2 * a;
-    
-    return ["X1 = " << x1 << ", X2 = " << x2 << endl];
-}
