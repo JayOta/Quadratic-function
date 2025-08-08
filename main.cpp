1e3b255de6b37044ad60fa1cdfd096ce39f9a3df
JayOta <otavio.cvilela@gmail.com>
2025-08-08 17:46:07 -0300

    em construção

--- main.cpp
+++ main.cpp
@@ -15,20 +15,20 @@ void show_delta(int a, int b, int c) {
  int delta = deltaFunc(a, b, c);
  int ac = a * c;
  int quatro_menos_ac = -4 * ac;
- map<int, string> passo1 = {{a, "a"}, {b, "b²"}, {c, "c"}};
- map<int, int> passo2 = {{a, a}, {b, b}, {c, c}};
- map<int, int> passo3 = {{a, a}, {b, pow(b, 2)}, {c, c}};
- map<int, int> passo4 = {{ac, ac}, {b, pow(b, 2)}};
+ map<string, string> passo1 = {{"a", "a"}, {"b", "b²"}, {"c", "c"}};
+ map<string, int> passo2 = {{"a", a}, {"b", b}, {"c", c}};
+ map<string, int> passo3 = {{"a", a}, {"b", pow(b, 2)}, {"c", c}};
+ map<string, int> passo4 = {{"ac", ac}, {"b2", pow(b, 2)}};
 
 
- cout << "Δ = " << passo1[b] << " - 4 * " << passo1[a] << " * " << passo1[c] << endl;
- cout << "Δ = " << passo2[b] << "² " <<"- 4 * " << passo2[a] << " * " << passo2[c] << endl;
- cout << "Δ = " << passo3[b] << " - 4 * " << passo3[a] << " * " << passo3[c] << endl;
+ cout << "Δ = " << passo1["b"] << " - 4 * " << passo1["a"] << " * " << passo1["c"] << endl;
+ cout << "Δ = " << passo2["b"] << "² " <<"- 4 * " << passo2["a"] << " * " << passo2["c"] << endl;
+ cout << "Δ = " << passo3["b"] << " - 4 * " << passo3["a"] << " * " << passo3["c"] << endl;
 
  if(quatro_menos_ac > 0 || quatro_menos_ac == 0){
-     cout << "Δ = " << passo4[b] << " + " << quatro_menos_ac << endl;
+     cout << "Δ = " << passo4["b2"] << " + " << quatro_menos_ac << endl;
  } else {
-     cout << "Δ = " << passo4[b] << quatro_menos_ac << endl;
+     cout << "Δ = " << passo4["b2"] << quatro_menos_ac << endl;
  }
  cout << "Δ = " << delta << endl;
 
@@ -49,17 +49,17 @@ void show_x1(int a, int b, int c){
     int delta_int = deltaFunc(a, b, c);
     double x1 = quadratic_func_x1(a, b, c);
     vector<string> parte = {"Δ","1.", "2.", "3.", "4.", "5."};
-    map<int, string> passo1 = {{a, "a"}, {b, "-b"}};
-    map<int, int> passo2 = {{a, a}, {b, b}, {delta_int, delta_int}};
-    map<int, int> passo3 = {{a, a}, {b, b}, {delta_int, sqrt(delta_int)}};
-    map<int, int> passo4 = {{a, a}, {b, b}, {delta_int, sqrt(delta_int)}};
-    map<int, int> passo5 = {{delta_int, delta_int}};
+    map<string, string> passo1 = {{"a", "a"}, {"b", "-b"}};
+    map<string, int> passo2 = {{"a", a}, {"b", b}, {"delta_int", delta_int}};
+    map<string, int> passo3 = {{"a", a}, {"b", b}, {"delta_int", sqrt(delta_int)}};
+    map<string, int> passo4 = {{"a", a}, {"b", b}, {"delta_int", sqrt(delta_int)}};
+    map<string, int> passo5 = {{"delta_int", delta_int}};
 
     cout << "x1:" << endl << endl;
-    cout << parte[1] << " x1 = " << "(" << passo1[b] << " + "<< "√" << parte[0] << ") " << "/ " << "(" << 2 << " * " << passo1[a] << ")" << endl;
-    cout << parte[2] << " x1 = " << "(" << "-(" << passo2[b] << ") + " << "√" << passo2[delta_int] << ") " << "/ " << "(" << 2 << " * " << passo2[a] << ")" << endl;
-    cout << parte[3] << " x1 = " << "(" << "-(" << passo3[b] << ") + " << passo3[delta_int] << ") " << "/ " << "(" << 2 << " * " << passo3[a] << ")" << endl;
-    cout << parte[4] << " x1 = " << "(" << "-(" << passo4[b] << ") + " << passo4[delta_int] << ") " << "/ " << 2 * passo4[a] << endl;
+    cout << parte[1] << " x1 = " << "(" << passo1["b"] << " + "<< "√" << parte[0] << ") " << "/ " << "(" << 2 << " * " << passo1["a"] << ")" << endl;
+    cout << parte[2] << " x1 = " << "(" << "-(" << passo2["b"] << ") + " << "√" << passo2["delta_int"] << ") " << "/ " << "(" << 2 << " * " << passo2["a"] << ")" << endl;
+    cout << parte[3] << " x1 = " << "(" << "-(" << passo3["b"] << ") + " << passo3["delta_int"] << ") " << "/ " << "(" << 2 << " * " << passo3["a"] << ")" << endl;
+    cout << parte[4] << " x1 = " << "(" << "-(" << passo4["b"] << ") + " << passo4["delta_int"] << ") " << "/ " << 2 * passo4["a"] << endl;
     cout << parte[5] << " x1 = " << setprecision(2) << x1 << endl;
   
 }
@@ -68,17 +68,17 @@ void show_x2(int a, int b, int c){
     int delta_int = deltaFunc(a, b, c);
     double x2 = quadratic_func_x2(a, b, c);
     vector<string> parte = {"Δ","1.", "2.", "3.", "4.", "5."};
-    map<int, string> passo1 = {{a, "a"}, {b, "-b"}};
-    map<int, int> passo2 = {{a, a}, {b, b}, {delta_int, delta_int}};
-    map<int, int> passo3 = {{a, a}, {b, b}, {delta_int, sqrt(delta_int)}};
-    map<int, int> passo4 = {{a, a}, {b, b}, {delta_int, sqrt(delta_int)}};
-    map<int, int> passo5 = {{delta_int, delta_int}};
+    map<string, string> passo1 = {{"a", "a"}, {"b", "-b"}};
+    map<string, int> passo2 = {{"a", a}, {"b", b}, {"delta_int", delta_int}};
+    map<string, int> passo3 = {{"a", a}, {"b", b}, {"delta_int", sqrt(delta_int)}};
+    map<string, int> passo4 = {{"a", a}, {"b", b}, {"delta_int", sqrt(delta_int)}};
+    map<string, int> passo5 = {{"delta_int", delta_int}};
 
     cout << "x2:" << endl << endl;
-    cout << parte[1] << " x2 = " << "(" << passo1[b] << " - "<< "√" << parte[0] << ") " << "/ " << "(" << 2 << " * " << passo1[a] << ")" << endl;
-    cout << parte[2] << " x2 = " << "(" << "-(" << passo2[b] << ") - " << "√" << passo2[delta_int] << ") " << "/ " << "(" << 2 << " * " << passo2[a] << ")" << endl;
-    cout << parte[3] << " x2 = " << "(" << "-(" << passo3[b] << ") - " << passo3[delta_int] << ") " << "/ " << "(" << 2 << " * " << passo3[a] << ")" << endl;
-    cout << parte[4] << " x2 = " << "(" << "-(" << passo4[b] << ") - " << passo4[delta_int] << ") " << "/ " << 2 * passo4[a] << endl;
+    cout << parte[1] << " x2 = " << "(" << passo1["b"] << " - "<< "√" << parte[0] << ") " << "/ " << "(" << 2 << " * " << passo1["a"] << ")" << endl;
+    cout << parte[2] << " x2 = " << "(" << "-(" << passo2["b"] << ") - " << "√" << passo2["delta_int"] << ") " << "/ " << "(" << 2 << " * " << passo2["a"] << ")" << endl;
+    cout << parte[3] << " x2 = " << "(" << "-(" << passo3["b"] << ") - " << passo3["delta_int"] << ") " << "/ " << "(" << 2 << " * " << passo3["a"] << ")" << endl;
+    cout << parte[4] << " x2 = " << "(" << "-(" << passo4["b"] << ") - " << passo4["delta_int"] << ") " << "/ " << 2 * passo4["a"] << endl;
     cout << parte[5] << " x2 = " << setprecision(2) << x2 << endl;
   
 }
